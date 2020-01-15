2c72e7bff617cde765b740412bb247a767072f7d
parkanajak <arnajaknp@gmail.com>
2020-01-16 05:11:30 +0700

    add backend structure

---
+++ MedicalApp/Project/server/routes/api.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+router.post('/sendform', function(req, res) {
+    axios({
+      url: 'http://localhost:8200/utility/idp',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(function(r) {
+        res.send({
+          request: {},
+          idp_list: r.data
+        })
+      }).catch(e => {
+        console.log(e);
+      });
+});
+
+router.post('/request', function(req, res) {
+  // console.log(req.body)
+  
+    if(save_to_db()) {
+        var reference_id = `ref_${generateRefId(10)}${generateRefId(10)}`
+        axios({
+            method: 'post',
+            url: `http://localhost:8200/rp/requests/citizen_id/${req.body.id}`,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            data: {
+              mode: 1,
+              node_id: 'rp1',
+              reference_id,
+              idp_id_list: [req.body.idp],
+              callback_url: `http://localhost:3000/api/callback`,
+              data_request_list: [],
+              request_message: `I would like to share my medical information to RP hospital (REF: ${reference_id})?`,
+              min_ial: 2.1,
+              min_aal: 2.1,
+              min_idp: 1,
+              request_timeout: 86400,
+              bypass_identity_check: false,
+            }
+          }).then(r => {
+            console.log(r)
+
+            res.send({
+              request_id: r.data.request_id,
+              initial_salt: r.data.initial_salt,
+              reference_id,
+            })
+
+        
+          }).catch(e => {
+            console.log(e)
+          })
+    } else {
+      res.status(500).send({
+        message: 'Cannot save data to database.'
+    });
+    }
+});
+
+function generateRefId(n) {
+    var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
+  
+    if ( n > max ) {
+            return generate(max) + generate(n - max);
+    }
+  
+    max        = Math.pow(10, n + add);
+    var min    = max/10; // Math.pow(10, n) basically
+    var number = Math.floor( Math.random() * (max - min + 1) ) + min;
+  
+    return ("" + number).substring(add); 
+}
+
+
+function save_to_db() {
+  return true;
+}
+
+router.post('/callback', function(req, res) {
+    console.log(req.body);
+    console.log('Received request callback from NDID API:', JSON.stringify(callbackData, null, 2));
+});
+
+module.exports = router;
